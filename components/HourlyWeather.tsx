190bb3062e94fc417a52ccfe096d310af98eca31
Arnie Hayes <arniehayes1@gmail.com>
2022-05-25 21:47:21 -0700

    added time for the temps

--- components/HourlyWeather.tsx
+++ components/HourlyWeather.tsx
@@ -6,6 +6,30 @@ const HourlyWeather = () => {
   const { hourlyForecast, alerts } = useContext(AddressContext);
 
   const newHourly = hourlyForecast.slice(0, 12);
+  const date = new Date();
+  const hour = date
+    .toLocaleString("en-US", {
+      timeZone: "America/Los_Angeles",
+      hour: "numeric",
+      hour12: true,
+    })
+    .replace(/AM|PM/, "");
+  const updatedHour = Number(hour);
+
+  const time = [
+    "Now",
+    updatedHour + 1,
+    updatedHour + 2,
+    updatedHour + 3,
+    updatedHour + 4,
+    updatedHour + 5,
+    updatedHour + 6,
+    updatedHour + 7,
+    updatedHour + 8,
+    updatedHour + 9,
+    updatedHour + 10,
+    updatedHour + 11,
+  ];
 
   useEffect(() => {
     console.log("hourly: ", hourlyForecast);
@@ -15,10 +39,17 @@ const HourlyWeather = () => {
   return (
     <div className={style["hourly-weather__container"]}>
       <div className={style["hourly-weather__list-container"]}>
-        <ul className={style["hourly-weather__list"]}>
+        <ul className={style["hourly-weather__list--time"]}>
+          {time.map((item: any, key: number) => (
+            <li key={key} className={style["hourly-weather__list--li-time"]}>
+              {item}
+            </li>
+          ))}
+        </ul>
+        <ul className={style["hourly-weather__list--temp"]}>
           {hourlyForecast.length > 0 &&
             newHourly.map((item, key: number) => (
-              <li key={key} className={style["hourly-weather__list--li"]}>
+              <li key={key} className={style["hourly-weather__list--li-temp"]}>
                 {Math.trunc(item.temp)}
               </li>
             ))}
