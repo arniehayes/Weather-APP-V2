b01bf7db46b5cab854408b2a65461bfcab835547
Arnie Hayes <arniehayes1@gmail.com>
2022-05-30 20:33:48 -0700

    ios fix

--- pages/_app.js
+++ pages/_app.js
@@ -9,9 +9,12 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0 viewport-fit=cover"
         />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style"
+            content="black-translucent" />
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap"
           rel="stylesheet"
