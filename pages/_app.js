45030c4a00af7b4f804b3a3741b9ec2f96db4c9a
Arnie Hayes <arniehayes1@gmail.com>
2022-05-31 20:43:57 -0700

    bg

--- pages/_app.js
+++ pages/_app.js
@@ -11,7 +11,7 @@ function MyApp({ Component, pageProps }) {
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0 viewport-fit=cover"
         />
-        <meta name="theme-color" content="#518acc" />
+        <meta name="theme-color" content={"#518acc"} />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black" />
         <link
